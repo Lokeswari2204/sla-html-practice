9cee108fe8e1d8e7863ea91f8b6ebb744948c71a
Lokeswari B <lokeswari390@gmail.com>
2026-09-07 21:57:20 +0530

    Add files via upload

---
+++ Day19/index.js
@@ -0,0 +1,42 @@
+// Q1. Declare variables using var, let, and const
+
+var student = "Lokeswari";
+let age = 22;
+const college = "PTU";
+
+console.log(student);
+console.log(age);
+console.log(college);
+
+
+// Q2. Create variables with string, number, boolean, null, undefined
+
+let studentName = "Lokeswari";   // String
+let marks = 85;                  // Number
+let isStudent = true;            // Boolean
+let address = null;              // Null
+let phoneNumber;                 // Undefined
+
+console.log(studentName);
+console.log(marks);
+console.log(isStudent);
+console.log(address);
+console.log(phoneNumber);
+
+
+// Q3. Use +, -, *, /, %, ==, ===, &&, ||
+
+let a = 20;
+let b = 5;
+
+console.log("Addition =", a + b);
+console.log("Subtraction =", a - b);
+console.log("Multiplication =", a * b);
+console.log("Division =", a / b);
+console.log("Modulus =", a % b);
+
+console.log(a == b);
+console.log(a === b);
+
+console.log(a > 10 && b < 10);
+console.log(a > 10 || b > 10);
